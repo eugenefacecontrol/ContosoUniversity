50094962a622581e40df240fe4ad619dde3a0b0b
Yauheni Sheima <ysh@leapwork.com>
2022-01-06 19:35:13 +0300

    Move Grade enum

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,21 +1,17 @@
 ﻿namespace ContosoUniversity.Models
 {
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
     public class Enrollment
     {
-        public enum Grade
-        {
-            A, B, C, D, F
-        }
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
 
-        public class Enrollment
-        {
-            public int EnrollmentID { get; set; }
-            public int CourseID { get; set; }
-            public int StudentID { get; set; }
-            public Grade? Grade { get; set; }
-
-            public Course Course { get; set; }
-            public Student Student { get; set; }
-        }
+        public Course Course { get; set; }
+        public Student Student { get; set; }
     }
 }
